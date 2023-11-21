fec542750ae9593c0417619b4146b425e1525ade
Abhay <abhaytripathi98.com>
2023-11-21 11:53:53 +0530

    small changes in front end

--- SEM_PROJECT/myproject/pedal/admin.py
+++ SEM_PROJECT/myproject/pedal/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Cycle, AppUser, Order, Payment, Rent, Transaction, Wallet, WalletTransaction, Reviews
 # Register your models here.
+admin.site.register(Payment)
+admin.site.register(WalletTransaction)
